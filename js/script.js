9cb060bf0618a05eaf7744f4914f26103f5fbcf8
yeremijesus9 <yeremijesus9@gmail.com>
2026-02-18 13:43:43 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Script principal del proyecto
+console.log('¡El proyecto está listo!');
+
+// Ejemplo de función
+function saludar(nombre) {
+    return `¡Hola, ${nombre}!`;
+}
+
+// Llamada de ejemplo
+console.log(saludar('Desarrollo Web'));
